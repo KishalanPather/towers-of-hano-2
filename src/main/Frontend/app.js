11c9eaf24553b82c0c1d659c39739d02522b035b
KishalanPather <ki5halanp4ther@gmail.com>
2025-01-23 00:11:38 +0200

    updated the css

--- src/main/Frontend/app.js
+++ src/main/Frontend/app.js
@@ -1,41 +1,46 @@
-////DOM elements 
-//const startBtn = document.querySelector(".start");
-//
-//
-////towers
-//let towers = []
-//
-////start game 
+//DOM elements 
+const startBtn = document.querySelector(".start");
+
+//towers
+let towers = []
+
+//start game 
 //startBtn.addEventListener("click", () => {
 //    startGame()
 //    .then(async () => {
 //        towers = await getTowers();
+//        renderTowers(towers);
 //    
 //    });
 //})
 
-
-const towers = [
-    {
-      towerNumber: 0,
-      stack: [
-        { value: 3 },
-        { value: 2 },
-        { value: 1 },
-      ],
-    },
-    {
-      towerNumber: 1,
-      stack: [],
-    },
-    {
-      towerNumber: 2,
-      stack: [
-        {value: 3},
-      ],
-    },
-  ];
-
-
-
+startBtn.addEventListener("click", async () => {
+  await startGame();
+  towers = await getTowers();
   renderTowers(towers);
+  
+  
+})
+
+
+
+//const towers = [
+//    {
+//      towerNumber: 0,
+//      stack: [
+//        { value: 3 },
+//        { value: 2 },
+//        { value: 1 },
+//      ],
+//    },
+//    {
+//      towerNumber: 1,
+//      stack: [],
+//    },
+//    {
+//      towerNumber: 2,
+//      stack: [
+//        {value: 3},
+//      ],
+//    },
+//  ];
