eb8c7ddfe585c916e7bd0fc44d76506b54738e17
KishalanPather <ki5halanp4ther@gmail.com>
2025-01-25 23:53:15 +0200

    added if statements for move buttons

--- src/main/Frontend/app.js
+++ src/main/Frontend/app.js
@@ -1,5 +1,6 @@
 //DOM elements 
 const startBtn = document.querySelector('.start');
+const moveBtns = document.querySelector('.moveBtns');
 const moveBtn = document.querySelector('.LM')
 
 //towers
@@ -23,6 +24,23 @@ startBtn.addEventListener('click', async () => {
   
   
 })
+
+moveBtns.addEventListener('click', (e) => {
+  if(e.target.matches('.LM')){
+    console.log('12')
+  } else if(e.target.matches('.LR')){
+    console.log('13')
+  }else if(e.target.matches('.ML')){
+    console.log('21')
+  }else if(e.target.matches('.MR')){
+    console.log('23')
+  }else if(e.target.matches('.RL')){
+    console.log('31')
+  }else if(e.target.matches('.RM')){
+    console.log('32')
+  }
+
+});
 
 moveBtn.addEventListener('click',() => {
   moveDisk("12");
