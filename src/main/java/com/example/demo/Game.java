8d172a59f6ac7ae9c7c9904a95167a05ef1062ff
KishalanPather <ki5halanp4ther@gmail.com>
2025-01-28 23:03:34 +0200

    updated how the counter works

--- src/main/java/com/example/demo/Game.java
+++ src/main/java/com/example/demo/Game.java
@@ -18,7 +18,7 @@ public class Game {
         this.auxTower = new Tower(1);
         this.endTower = new Tower(2);
         this.functionality = new Functionality();
-        this.numberOfDisks = 3; // Default number of disks
+        this.numberOfDisks = 2; // Default number of disks
         this.running = false;
     }
 
@@ -77,6 +77,11 @@ public class Game {
                 return response;
         }
 
+        // check if we can increment the counter to response
+        if(response.validMove){
+            counter++;
+        }
+
         //then assign if the game was completed in response
         if (functionality.checkCompleted(endTower, numberOfDisks)) {
             this.running = false;
@@ -87,10 +92,6 @@ public class Game {
             response.gameCompleted = false;
         }
 
-        // check if we can increment the counter to response
-        if(response.validMove){
-            counter++;
-        }
         response.counter = counter; //finally, assign the counter to response
         return response;
     }
